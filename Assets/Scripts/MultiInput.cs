7fee2a5142d832037c54c5cb93c5604363d34bc5
cafeunder <cafe3641@gmail.com>
2016-07-05 01:27:50 +0900

    ゆにてぃちゃん！

---
+++ Assets/Scripts/MultiInput.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MultiInput {
+	private static Vector3 touchPosition = Vector3.zero;
+
+	public static bool Touched(){
+	#if UNITY_EDITOR || UNITY_STANDALONE_WIN || UNITY_STANDALONE_OSX || UNITY_STANDALONE_LINUX
+		return (Input.GetMouseButtonDown(0) || Input.GetMouseButton(0) || Input.GetMouseButtonUp(0));
+	#elif UNITY_IPHONE || UNITY_ANDROID
+		return Input.touchCount > 0;
+	#endif
+		return false;
+	}
+
+	public static TouchPhase GetPhase(){
+	#if UNITY_EDITOR || UNITY_STANDALONE_WIN || UNITY_STANDALONE_OSX || UNITY_STANDALONE_LINUX
+		if(Input.GetMouseButtonDown(0)){ 
+			return TouchPhase.Began;
+		}
+		if(Input.GetMouseButton(0)){
+			return TouchPhase.Moved;
+		}
+		if(Input.GetMouseButtonUp(0)){
+			return TouchPhase.Ended;
+		}
+	#elif UNITY_IPHONE || UNITY_ANDROID
+		if(Input.touchCount > 0){
+			return Input.GetTouch(0).phase;
+		}
+	#endif
+
+		return TouchPhase.Canceled;
+	}
+
+    public static Vector3 GetPosition()
+    {
+	#if UNITY_EDITOR || UNITY_STANDALONE_WIN || UNITY_STANDALONE_OSX || UNITY_STANDALONE_LINUX
+		if(MultiInput.Touched()){ return Input.mousePosition; }
+	#elif UNITY_IPHONE || UNITY_ANDROID
+		if(MultiInput.Touched()){
+			Touch touch = Input.GetTouch(0);
+			touchPosition.x = touch.position.x;
+			touchPosition.y = touch.position.y;
+			return touchPosition;
+		}
+	#endif
+        return Vector3.zero;
+    }
+}
---
+++ Assets/Scripts/TouchTest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchTest : MonoBehaviour {
+	public GUIText text;
+	
+	// Update is called once per frame
+	void Update () {
+		if(MultiInput.Touched()){
+			Color color = new Color(1,1,1,1.0f);
+			text.color = color;
+			text.text = MultiInput.GetPhase()+"";
+		} else {
+			Color color = new Color(0,0,0,0.0f);
+			text.color = color;
+		}
+	}
+}
