1ee9aa51397382a5cd747310d5638def01ecd45a
cafeunder <cafe3641@gmail.com>
2016-07-05 19:48:45 +0900

    シーン遷移の追加

---
+++ Assets/Scripts/Main/MainRoot.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainRoot : MonoBehaviour {
+	void Update () {
+		if(MultiInput.Touched()){
+			Application.LoadLevel("Result");
+		}		
+	}
+}
---
+++ Assets/Scripts/Result/ResultRoot.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResultRoot : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(MultiInput.Touched()){
+			Application.LoadLevel("Title");
+		}
+	}
+}
